5c4dec3c9fb6443cc58549a31e3abb5a5b762f0e
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-03-18 16:58:18 +0530

    added the zod validation for user sign up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,20 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken';
-import z from 'zod'
+import {z} from 'zod'
 import { User, Tag, Link, Content } from './db';
 const app = express();
 import {JWT_KEY} from './config'
 import { userMiddleware } from './middleware';
+import { ContextExclusionPlugin } from 'webpack';
 
 app.use(express.json());
 
 
-
+const userSchema = z.string().min(3).max(10)
+const passwordSchema = z.string().min(8, {message: "password must be atleast 8 characters"}).max(20, {message: "password must not be more than 20 characters"}).regex(/[A-Z]/, {message: "password must have atleast 1 uppercase letter"}).regex(/[a-z]/, {message: "password must have atleast 1 upper case letter"}).regex(/[0-9]/, {message: "password must containt atleast 1 number"}).regex(/[^a-zA-Z0-9]/, {message: "password must contain atleast 1 special character"})
 
 app.post('/api/v1/signup', async (req, res) => {
 	const { username, password } = req.body;
-	if (username && password) {
+	const validUsername = userSchema.safeParse(username)
+	const validPassword = passwordSchema.safeParse(password)
+	if (validUsername.success == true && validPassword.success == true) {
 		const existingUser = await User.findOne({ username });
         console.log(existingUser)
 		if (existingUser) {
@@ -29,12 +33,16 @@ app.post('/api/v1/signup', async (req, res) => {
 					username: username,
 					password: hashedPassword
 				});
+				res.status(200).json("user created successfully")
+				return;
 			} catch (error) {
 				res.status(500).json(error);
 			}
 		}
 	} else {
-		res.status(411).json('enter the required fields.');
+		res.status(411).json({
+			err: validUsername.error, 
+			err2: validPassword.error});
 	}
 });
 
@@ -88,7 +96,14 @@ app.post('/api/v1/content',userMiddleware, async(req, res) => {
 	}
 });
 
-app.get('/api/v1/content', (req, res) => {});
+app.get('/api/v1/content',userMiddleware, async(req, res) => {
+	// @ts-ignore
+	const userId = req.userId
+	const userContent = await Content.find({
+		userId
+	}).populate("userId", "username")
+	res.json({userContent})
+});
 
 app.delete('/api/v1/content', (req, res) => {});
 
